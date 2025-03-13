673d24b24cc70d3e62659312c5e8e44197be42ad
ahmedobeidi <alobeidi.dev@gmail.com>
2025-03-13 14:07:36 +0100

    ADD: Registration Regex and Mailtrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,32 +5,54 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'id' => "email",
+                    'required' => true,
+                    'class' => 'border border-off-gray rounded p-2 focus:border-2 border-opacity-25',
+                    'placeholder' => 'Email'
+                ],
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+                        'message' => 'Please enter a valid email',
+                    ])
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr'  => [
+                        'class' => 'border border-off-gray rounded p-2 focus:border-2 border-opacity-25',
+                        'placeholder' => 'Password'
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr'  => [
+                        'class' => 'border border-off-gray rounded p-2 focus:border-2 border-opacity-25',
+                        'placeholder' => 'Repeat Password'
+                    ],
+                ],
+                'invalid_message' => 'The password fields must match.',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -38,12 +60,14 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/',
+                        'message' => 'Your password must contain uppercase, lowercase, number, and be at least 6 characters long.',
+                    ]),
                 ],
-            ])
-        ;
+        ]   );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
