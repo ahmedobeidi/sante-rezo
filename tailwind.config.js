908f8e17d7d2452594e739f6f2d4e6735ed54d9b
ahmedobeidi <alobeidi.dev@gmail.com>
2025-04-03 12:23:45 +0200

    ADD: patient profile frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         'dark-gray': '#2c2d3f',
         'off-blue': '#298BE9',
         'off-gray': '#4B5563',
-        'off-white': '#f9f9f9'
+        'off-white': '#f9f9f9',
+        'off-test': '#edf6f9'
       },
       screens: {
         '3xl': '2400px',
