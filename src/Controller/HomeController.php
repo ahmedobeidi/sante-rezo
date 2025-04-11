3087febc83af368d84ae3217f47d3545ece15258
ahmedobeidi <alobeidi.dev@gmail.com>
2025-04-11 16:23:43 +0200

    ADD: get patients count and doctors count

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\DoctorRepository;
+use App\Repository\PatientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,13 +11,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(DoctorRepository $doctorRepository, PatientRepository $patientRepository): Response
     {
         // Check if the user has the ROLE_ADMIN role
         if ($this->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('app_admin');
         }
+
+        // Fetch the counts of doctors and patients
+        $doctorCount = $doctorRepository->count([]);
+        $patientCount = $patientRepository->count([]);
         
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'doctorCount' => $doctorCount,
+            'patientCount' => $patientCount,
+        ]);
     }
 }
